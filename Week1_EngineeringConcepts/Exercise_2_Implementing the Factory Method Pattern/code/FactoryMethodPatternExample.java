542f5aa677b6505271a148444830d31e5b28957f
Sai premjeet Das <121485126+Sai-premjeet-Das@users.noreply.github.com>
2025-06-21 20:49:12 +0530

    Design pattern Exercise 2

---
+++ Week1_EngineeringConcepts/Exercise_2_Implementing the Factory Method Pattern/code/FactoryMethodPatternExample.java
@@ -0,0 +1,55 @@
+interface Document {
+    void open();
+}
+class WordDocument implements Document {
+    public void open() {
+        System.out.println("WordDocument called -- Opening Word document.");
+    }
+}
+class PdfDocument implements Document {
+    public void open() {
+        System.out.println(" PdfDocument called -- Opening PDF document.");
+    }
+}
+class ExcelDocument implements Document {
+    public void open() {
+        System.out.println(" ExcelDocument called -- Opening Excel document.");
+    }
+}
+abstract class DocumentFactory {
+    public abstract Document createDocument();
+}
+class WordDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        System.out.println("Creating Word document.");
+        return new WordDocument();
+    }
+}
+class PdfDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        System.out.println("Creating PDF document.");
+        return new PdfDocument();
+    }
+}
+class ExcelDocumentFactory extends DocumentFactory {
+    public Document createDocument() {
+        System.out.println("Creating Excel document.");
+        return new ExcelDocument();
+    }
+}
+
+public class FactoryMethodPatternExample {
+    public static void main(String[] args) {
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document word = wordFactory.createDocument();
+        word.open();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdf = pdfFactory.createDocument();
+        pdf.open();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excel = excelFactory.createDocument();
+        excel.open();
+    }
+}
